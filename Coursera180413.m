eb8923298a16b4036ded07b9dacec5ce3f780c4b
zhuangbin <zhuangbin@esxvm1606>
2018-04-26 09:53:29 +0800

    20180425

---
+++ Coursera180413.m
@@ -0,0 +1,3 @@
+figure(1), imagesc(magic(100)), colorbar, colormap gray;
+A = randn(5000,5);
+figure(2), hist(A)
---
+++ FirstOne.m
@@ -0,0 +1,12 @@
+#x = -10:0.01:10
+#y = sin(x)
+#z = cos(x)
+#plot3(y,z,x)
+
+x = -1:0.01:1
+y = (-sqrt(1-power(x,2))+sqrt(abs(x)))*4/5
+plot(x,y)
+hold on
+y = (sqrt(1-power(x,2))+sqrt(abs(x)))*4/5
+hold on
+plot(x,y)
