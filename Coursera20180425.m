06c6474f2471d65dfc316e0dd42a92f6b5aa37c4
zhuangbin <zhuangbin@zhuangbin-Lenovo-K4450>
2018-04-26 15:48:52 +0800

    20180425

---
+++ Coursera20180425.m
@@ -0,0 +1,5 @@
+v=zeros(10,1);
+for i = 1:10;
+  v(i)=i^2;
+end;
+v
--- FirstOne.m
+++ FirstOne.m
@@ -3,10 +3,10 @@
 #z = cos(x)
 #plot3(y,z,x)
 
-x = -1:0.01:1
-y = (-sqrt(1-power(x,2))+sqrt(abs(x)))*4/5
+x = -1:0.01:1;
+y = (-sqrt(1-power(x,2))+sqrt(abs(x)))*4/5;
 plot(x,y)
 hold on
-y = (sqrt(1-power(x,2))+sqrt(abs(x)))*4/5
+y = (sqrt(1-power(x,2))+sqrt(abs(x)))*4/5;
 hold on
 plot(x,y)
