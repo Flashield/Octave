8b7c284e14e83e6b3380f906cef89dcc85a2db51
zhuangbin <zhuangbin@zhuangbin-Lenovo-K4450>
2018-04-27 21:05:23 +0800

    #Control Statment and Funtion

--- Coursera20180425.m
+++ Coursera20180425.m
@@ -1,5 +1,34 @@
+#Control Statment While, For , if elseif else
 v=zeros(10,1);
-for i = 1:10;
+i=1;
+while i <= 5;
+  v(i)=i^3;
+  i++;
+end;
+disp(v);
+i=1;
+while true,
+  v(i)=i^4;
+  i++;
+  if i==7,
+    break;
+  end;
+end;
+disp(v);
+
+for i = 1:10,
   v(i)=i^2;
 end;
-v
+disp(v)
+
+for i = 1:1:10;
+  disp(i)
+end;
+
+if v(3)==4,
+  disp('The Value is 4');
+elseif v(3) == 9,
+  disp('The Value is 9');
+else
+  disp('The Value is not 4 or 8');
+end;
---
+++ TryFunction.m
@@ -0,0 +1,2 @@
+function y=TryFunction(x)
+y = x^2;
